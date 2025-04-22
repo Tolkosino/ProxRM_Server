228e46f7e5edb056416222688816717e0ef7b1c1
Tolkosino <35376652+Tolkosino@users.noreply.github.com>
2025-04-22 20:30:49 +0200

    Removed useless debug log message (#19)

--- src/classes/commands/get_vmlist.py
+++ src/classes/commands/get_vmlist.py
@@ -5,7 +5,6 @@ class get_vmlist(CommandBase):
     def execute(self, **kwargs):
         from classes.db.user import DB_User
         
-        self.logger.debug("")
         session_id = kwargs.get("session_id")
         user_id = DB_User().get_user_id_by_session_id(session_id)
         vms = DB_User().get_vms_for_perms_of_user(user_id)
