cce21a4f5efcb29a6b191315dd66418fcee547f8
Tolkosino <35376652+Tolkosino@users.noreply.github.com>
2025-06-08 21:06:40 +0200

    fixing import (#22)
    LGTM, gj

--- src/classes/db/user.py
+++ src/classes/db/user.py
@@ -1,5 +1,5 @@
-from src.classes.db.helper import DatabaseConnection
-from src.classes.config.config import Config
+from classes.db.helper import DatabaseConnection
+from classes.config.config import Config
 import uuid
 import bcrypt
 import logging
