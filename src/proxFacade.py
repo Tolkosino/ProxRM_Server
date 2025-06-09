cbb84dd30cdd5041d2e98fbbbd74291d494530fa
Tolkosino <35376652+Tolkosino@users.noreply.github.com>
2025-06-09 02:56:04 +0200

    fix hostaction (#34)
    * fix hostaction
    
    * Merge branch 'master' into fix_hostaction

--- src/proxFacade.py
+++ src/proxFacade.py
@@ -24,9 +24,9 @@ class ProxFacade:
                 self.logger.debug(f"{userId} has permissions")
                 command = CommandFactory.create_command(command)
                 res = command.execute(vmid=vmid, action=action, session_id=session_id)
-            elif userId and command == "get_vmlist":                
+            elif userId and command in ["set_hostaction","get_vmlist"]:
                 command = CommandFactory.create_command(command)
-                res = command.execute(action=action, session_id=session_id)
+                res = command.execute(vmid=vmid, action=action, session_id=session_id)
             else:
                 return "Access Denied"
             
